6aae7e448ec7420887f3b9ced8d78ec443a7e33d
JOGINDER571 <snshrma7@gmail.com>
2024-01-28 16:04:20 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,22 +1,29 @@
-import express from "express"
+import express from "express";
 import dotENV from "dotenv";
 import startServer from "./server.js";
 import router from "./routes/rootRouter.js";
 import bodyParser from "body-parser";
-import cors from 'cors';
-const app=express();
+import cors from "cors";
+const app = express();
+app.use(
+  cors({
+    origin: ["https://insta-frontend-pi.vercel.app"],
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 //  Importing dotENV file
-dotENV.config({ 
-    path: "./config/config.env"         
+dotENV.config({
+  path: "./config/config.env",
 });
 
-app.get('/',(req,res)=>{
-    res.send("home page");
+app.get("/", (req, res) => {
+  res.send("home page");
 });
-app.use("/user",router);
+app.use("/user", router);
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,4 +31,3 @@ app.use(bodyParser.urlencoded({ extended: true }));
 export default app;
 
 startServer();
-
